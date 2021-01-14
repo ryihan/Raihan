9a2305180e01a0c8d670c0d71a1b6c1a38b7f644
raihan <ryihan55@gmail.com>
2021-01-14 22:24:27 +0600

    Update

--- resources/views/Common-Asset/css-js-top.blade.php
+++ resources/views/Common-Asset/css-js-top.blade.php
@@ -1,5 +1,7 @@
 <!--===============================CSS=======================================-->
 <!---------------------------------Bootstrap----------------------------------->
+<link rel="stylesheet" href="{{ asset('css/Bootstrap/Bundel/bootstrap.min.css') }}" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<?php /*
 <link rel="stylesheet" href="{{ asset('css/Bootstrap/bootstrap-grid.css') }}">
 <link rel="stylesheet" href="{{ asset('css/Bootstrap/bootstrap-grid.css.map') }}">
 <link rel="stylesheet" href="{{ asset('css/Bootstrap/bootstrap-grid.min.css') }}">
@@ -12,6 +14,7 @@
 <link rel="stylesheet" href="{{ asset('css/Bootstrap/bootstrap.css.map') }}">
 <link rel="stylesheet" href="{{ asset('css/Bootstrap/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ asset('css/Bootstrap/bootstrap.min.css.map') }}">
+*/?>
 <!---------------------------------Custom----------------------------------->
 <link rel="stylesheet" href="{{ asset('css/Custom/min.css') }}">
 <!--===============================JS=======================================-->
